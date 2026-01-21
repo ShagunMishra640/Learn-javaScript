067b25a33991710fc2f205ec4fb59a5651348548
ShagunMishra640 <shagunmishra640@gmail.com>
2026-01-21 13:07:46 +0530

    add this file

---
+++ 05_iterations/whileloop.js
@@ -0,0 +1,35 @@
+// while
+
+
+// initialization
+// while (condition) {
+//     Code
+//     increment/decrement
+// }
+
+let index=0
+while(index<=10) {
+    console.log(index);
+    index = index+2;
+}
+
+
+const myArray=['flash', 'batman', 'superman']
+let arr=0
+while (arr<myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr++
+
+    
+}
+// do {
+    
+// } while (condition);
+
+
+let score=1
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score<=10); // it will run atleast one time even if condition false
